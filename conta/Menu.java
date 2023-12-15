68f2eac628e514079c8f3f452552e31affcd8e77
Lucas Nakamura <lucasitironakamura@gmail.com>
2023-12-15 14:06:26 -0300

    first commit

---
+++ conta/Menu.java
@@ -0,0 +1,12 @@
+package conta;
+import java.util.Scanner;
+
+import conta.util.Cores;
+public class Menu {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+	}
+}
